8fe7f669f0ecca5c7dd96dbb837b17f9c7dfd23f
Bala Luguja <bluguja@gmail.com>
2020-02-19 19:42:45 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def levitation_force
   levitation_force = 6
-
-while levitation_force <=10 #your code here
+  
+while levitation_force !=10 #your code here
   puts "Wingardium Leviosa"
-    
+  
     levitation_force += 1
   end
 end
